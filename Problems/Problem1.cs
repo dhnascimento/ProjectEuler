3113a054cf087c74fc35db7056b9d216dc68f4ac
danihermes <danielhn@gmail.com>
2024-11-01 07:58:51 -0400

    first commit

---
+++ Problems/Problem1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ProjectEuler.Problems
+{
+    public class Problem1
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine($"The sum of all multiples of 3 or 5 below 1000 is {sum}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ProjectEuler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please specify the problem number to run.");
+                return;
+            }
+
+            string problemNumber = args[0];
+            string className = $"ProjectEuler.Problems.Problem{problemNumber}";
+
+            try
+            {
+                Type type = Assembly.GetExecutingAssembly().GetType(className);
+                MethodInfo? method = type?.GetMethod("Main", BindingFlags.Public | BindingFlags.Static);
+                method?.Invoke(null, new object[] { new string[] { } });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
